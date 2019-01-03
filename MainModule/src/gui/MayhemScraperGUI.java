8c6b2562264dd9e851cd9cd95ef1cb6feba80a8d
Ihor <ihorbodia@gmail.com>
2019-01-03 21:20:18 +0100

    GUI improvements.

--- MainModule/src/gui/MayhemScraperGUI.java
+++ MainModule/src/gui/MayhemScraperGUI.java
@@ -1,21 +1,16 @@
 package gui;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class MayhemScraperGUI extends JFrame {
     private JPanel mainPanel;
     private JButton runButton;
+    private JLabel labelStatus;
+    private JLabel labelStatusValue;
 
     public MayhemScraperGUI() {
         this.setContentPane(mainPanel);
-        runButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
+        runButton.addActionListener(e -> System.out.println("Button clicked"));
         mainPanel.setVisible(true);
     }
 }
--- MainModule/src/logic/MayhemScraper.java
+++ MainModule/src/logic/MayhemScraper.java
@@ -2,12 +2,25 @@ package logic;
 
 import gui.MayhemScraperGUI;
 
+import javax.swing.*;
+
 public class MayhemScraper {
 
     public static void main(String[] args) {
         MayhemScraperGUI gui = new MayhemScraperGUI();
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
         gui.setTitle("Mayhem scraper v 1.0");
         gui.setVisible(true);
         gui.setResizable(false);
+        gui.setSize(400, 150);
     }
 }
