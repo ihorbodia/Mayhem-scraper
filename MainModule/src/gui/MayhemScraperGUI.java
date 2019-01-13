4845b24a0917cadebc3d7c2781dccb436fb79ec6
Ihor <ihorbodia@gmail.com>
2019-01-13 15:10:42 +0100

    Added properties file.

--- MainModule/src/gui/MayhemScraperGUI.java
+++ MainModule/src/gui/MayhemScraperGUI.java
@@ -13,10 +13,12 @@ public class MayhemScraperGUI extends JFrame {
     public MayhemScraperGUI() {
         this.setContentPane(mainPanel);
         MayhemScraperLogic logicObj = new MayhemScraperLogic();
+        logicObj.restoreProperties();
         runButton.addActionListener(e -> {
             System.out.println("Button clicked");
             logicObj.Run();
         });
         mainPanel.setVisible(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
--- MainModule/src/logic/MayhemScraperLogic.java
+++ MainModule/src/logic/MayhemScraperLogic.java
@@ -1,11 +1,14 @@
 package logic;
 
 import logic.Models.ScrapedModelItem;
+import org.apache.commons.io.FilenameUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
-import java.io.IOException;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class MayhemScraperLogic {
 
@@ -13,16 +16,24 @@ public class MayhemScraperLogic {
     //private final int maximumIndex = 9999999;
     private final int maximumIndex = 500;
     private int currentIndex = 1;
+    File propertiesFile;
+    Boolean IsWorked;
+
+    public Properties properties = new Properties();
 
     public void Run() {
         resultItems = new ArrayList();
         while (currentIndex < maximumIndex) {
+            IsWorked = true;
             String URL = prepareURL();
             Element body = scrapeCurrentPage(URL);
             resultItems.add(new ScrapedModelItem(body, currentIndex));
+            saveProperties();
             currentIndex++;
         }
         currentIndex = 1;
+        IsWorked = false;
+        saveProperties();
     }
 
     private String prepareURL() {
@@ -37,5 +48,95 @@ public class MayhemScraperLogic {
             e.printStackTrace();
         }
         return doc;
+    }
+
+    public void saveProperties() {
+        OutputStream output = null;
+        try {
+            output = new FileOutputStream(propertiesFile.getAbsoluteFile());
+            properties.setProperty("index", String.valueOf(currentIndex));
+            properties.setProperty("IsWorked", String.valueOf(IsWorked));
+            properties.store(output, null);
+        } catch (IOException io) {
+            io.printStackTrace();
+            System.out.println(io.getMessage());
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    private void createNewFile() {
+        OutputStream output = null;
+        try {
+            File propertiesFileTemp = File.createTempFile("configMS", ".properties");
+            String propPath = FilenameUtils.getFullPathNoEndSeparator(propertiesFileTemp.getAbsolutePath()) + File.separator + "configMS.properties";
+            File f = new File(propPath);
+            if (f.exists() && !f.isDirectory()) {
+                propertiesFile = f;
+            } else {
+                propertiesFile = f;
+                f.createNewFile();
+                output = new FileOutputStream(propertiesFile.getAbsoluteFile());
+                properties.setProperty("index", "1");
+                properties.setProperty("IsWorked", "false");
+                properties.store(output, null);
+            }
+            propertiesFileTemp.delete();
+        } catch (IOException io) {
+            io.printStackTrace();
+            System.out.println(io.getMessage());
+        } finally {
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void restoreProperties() {
+        InputStream input = null;
+        try {
+            createNewFile();
+            input = new FileInputStream(propertiesFile.getAbsoluteFile());
+            properties.load(input);
+
+            if (properties.get("index") != null) {
+                String currentIndexStr = properties.get("index").toString();
+                currentIndex = Integer.parseInt(currentIndexStr);
+            }
+
+            if (properties.get("IsWorked") != null) {
+                String isWorkStr = properties.get("IsWorked").toString();
+                IsWorked = Boolean.parseBoolean(isWorkStr);
+            }
+
+            if (IsWorked) {
+                Run();
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
