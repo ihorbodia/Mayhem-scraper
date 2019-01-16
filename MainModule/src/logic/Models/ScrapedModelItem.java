f385bd421fc4e95aa1fb0441b8b85c28dd616095
Ihor <ihorbodia@gmail.com>
2019-01-16 23:53:37 +0100

    Done with FB or IG links search.

--- MainModule/src/logic/Models/ScrapedModelItem.java
+++ MainModule/src/logic/Models/ScrapedModelItem.java
@@ -1,6 +1,10 @@
 package logic.Models;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Element;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ScrapedModelItem {
     public final int Id;
@@ -8,6 +12,7 @@ public class ScrapedModelItem {
     public String Location;
     public String WebsiteURL;
     public boolean isObjectCorrect = true;
+    public String Description;
 
     public ScrapedModelItem(Element body, int id) {
         Id = id;
@@ -27,6 +32,19 @@ public class ScrapedModelItem {
                 Location = body.select("div.location").text();
                 ModelName = body.select("#user_name").text();
                 WebsiteURL = body.select("div.website_url").text();
+                Description = body.select("#about-me-container").text();
+                if (StringUtils.isEmpty(WebsiteURL)) {
+                    Pattern igPattern = Pattern.compile("(instagram\\.com\\/([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\\.(?!\\.))){0,28}(?:[A-Za-z0-9_]))?))|(@([a-z0-9_]{1,255}))");
+                    Pattern fbPattern = Pattern.compile("(?:https?:\\/\\/)?(?:www\\.)?(?:facebook|fb|m\\.facebook)\\.(?:com|me)\\/(?:(?:\\w)*#!\\/)?(?:pages\\/)?(?:[\\w\\-]*\\/)*([\\w\\-\\.]+)(?:\\/)?");
+                    Matcher igMatcher = igPattern.matcher(Description);
+                    Matcher fbMatcher = fbPattern.matcher(Description);
+                    if (igMatcher.find()) {
+                        WebsiteURL = igMatcher.group(0);
+                    }
+                    else if (fbMatcher.find()) {
+                        WebsiteURL = fbMatcher.group(0);
+                    }
+                }
                 return;
             }
             else {
